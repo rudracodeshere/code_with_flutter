6d863bcd3e5e0368a0d616fd363fe60eca5959e6
Rudra Sharma <rudracollegeclass@gmail.com>
2024-02-11 23:07:58 +0530

    Learned adding image assets and icons

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hello World!"),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+            title: const Text('I AM RICH'), backgroundColor: Colors.black26),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
